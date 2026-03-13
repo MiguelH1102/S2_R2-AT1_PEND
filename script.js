8665d14bf55e701b7ff876f1c863c029a6d744b4
MIGUEL HENRI GOMES NUNES <miguel.nunes9@senaisp.edu.br>
2026-03-13 13:15:03 -0300

    AT6

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botao = document.getElementById("btnAdicionar");
 const mensagem = document.getElementById("mensagem");
 const lista = document.getElementById("listaTarefas");
 
-botao.addEventListener("click", function () {
+botao.addEventListener("click",  ()=> {
 
     const texto = input.value.trim();
 
@@ -13,28 +13,53 @@ botao.addEventListener("click", function () {
         return;
     }
 
+  
     const novaTarefa = document.createElement("li");
     novaTarefa.className = "list-group-item d-flex justify-content-between align-items-center";
 
+   
+    const container = document.createElement("div");
+    container.className = "d-flex align-items-center gap-2";
+
+   
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input";
+
+    
     const span = document.createElement("span");
     span.textContent = texto;
 
+    
+    checkbox.addEventListener("change",  ()=> {
+        if (checkbox.checked) {
+            span.style.textDecoration = "line-through";
+            span.style.color = "gray";
+        } else {
+            span.style.textDecoration = "none";
+            span.style.color = "black";
+        }
+    });
+
+   
     const btnRemover = document.createElement("button");
     btnRemover.textContent = "Remover";
     btnRemover.className = "btn btn-danger btn-sm";
 
-    btnRemover.addEventListener("click", function () {
+    btnRemover.addEventListener("click",  ()=> {
         lista.removeChild(novaTarefa);
     });
 
-    novaTarefa.appendChild(span);
-    novaTarefa.appendChild(btnRemover);
+    container.appendChild(checkbox);
+    container.appendChild(span);
 
+    novaTarefa.appendChild(container);
+    novaTarefa.appendChild(btnRemover);
 
     lista.appendChild(novaTarefa);
 
     input.value = "";
 
-    mensagem.textContent = "Tarefa adicionado com sucesso!";
+    mensagem.textContent = "Tarefa adicionada com sucesso!";
     mensagem.className = "text-success fw-bold";
-})
+});
